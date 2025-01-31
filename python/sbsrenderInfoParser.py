85a819aea1c7fefdfb150286be327658dde654cc
Jooyoung Lee <jooyounglee93@gmail.com>
2025-01-31 21:09:11 +0900

    중간 서밋.

--- python/sbsrenderInfoParser.py
+++ python/sbsrenderInfoParser.py
@@ -1,6 +1,7 @@
-from subprocess import run, CompletedProcess
+from subprocess import run, CompletedProcess, CREATE_NO_WINDOW
 from pathlib import Path
 from enum import Enum
+from queue import Queue
 
 from pysbs.context import Context
 from pysbs.sbsenum import InputValueTypeEnum
@@ -14,50 +15,127 @@ def PrintDictRecur(d: dict, indent=0):
             print(f"{' ' * indent}{k}:{v}")
 
 
+class GraphInfo:
+    __name: str
+
+    def __init__(self, graphInfo: str):
+        lines = list(map(lambda x: x.strip(), graphInfo.split('\r\n')))
+        self.__name = str(lines.pop(0).split('//')[-1].strip())
+        inputs = []
+        outputs = []
+        presets = []
+        graphusages = set()
+        self.__usageOverride = False
+
+        for l in lines:
+            toks = l.split(' ')
+            if toks[0] == 'INPUT':
+                inputs.append((toks[1], toks[2]))
+            elif toks[0] == 'OUTPUT':
+                lenTok = len(toks)
+                if lenTok == 2:
+                    outputs.append(toks[1])
+                elif lenTok == 3:
+                    usages = toks[2].split(',')
+                    for u in usages:
+                        self.__usageOverride |= u in graphusages
+                        graphusages.add(u)
+                    outputs.append((toks[1], usages))
+            elif toks[0] == 'PRESET':
+                presets.append(toks[1])
+
+        self.__inputs = tuple(inputs)
+        self.__outputs = tuple(outputs)
+        self.__presets = tuple(presets)
+        self.__usages = tuple(graphusages)
+
+    @property
+    def Name(self):
+        return self.__name
+
+    @property
+    def Input(self):
+        return self.__inputs
+
+    @property
+    def Output(self):
+        return self.__outputs
+
+    @property
+    def Preset(self):
+        return self.__presets
+
+    @property
+    def HasPreset(self):
+        return len(self.__presets) > 0
+
+    @property
+    def Usage(self):
+        return self.__usages
+
+    def __str__(self):
+
+        toStr = [self.__name,
+                 ' Inputs:',
+                 '\n'.join([f"  {x[1]} : {x[0]}" for x in self.__inputs]),
+                 ' Outputs:',
+                 '\n'.join([f"  {x[0]} for ({', '.join(x[1])})" for x in self.__outputs])
+                 ]
+
+        if len(self.__presets) > 0:
+            presets = ', '.join(self.__presets)
+            toStr.append(f' Presets: {presets}')
+
+        if len(self.__usages) > 0:
+            usages = ', '.join(self.__usages)
+            toStr.append(f' Usages: {usages}')
+
+        return '\n'.join(toStr)
+
+
+class SBSARInfo(dict):
+    def __init__(self, sbsarPath: str):
+        super().__init__()
+        self.__sbsarPath = Path(sbsarPath)
+        installPath = Path(Context.getAutomationToolkitInstallPath())
+        sbsrender_exe = installPath.joinpath("sbsrender.exe")
+        sbsar = sbsarPath
+
+        result: CompletedProcess = run([sbsrender_exe, "info", sbsar], capture_output=True,
+                                       creationflags=CREATE_NO_WINDOW)
+        infoByte = result.stdout.decode()
+        infos = tuple(infoByte.strip().split('\r\n\r\n'))
+        for g in infos:
+            graphInfo = GraphInfo(g)
+            self[graphInfo.Name] = graphInfo
+
+    @property
+    def GraphNames(self):
+        return tuple(self.keys())
+
+    @property
+    def Graphs(self):
+        return tuple(self.values())
+
+    @property
+    def Path(self):
+        return self.__sbsarPath
+
+    def __str__(self):
+        toStr = [f'SBSAR: {self.__sbsarPath}']
+        for k, v in self.items():
+            toStr.append(str(v))
+        return '\n'.join(toStr)
+
+    def __getitem__(self, item) -> GraphInfo:
+        return super().__getitem__(item)
+
+
 def GetSBSARInfo(sbsarPath: str):
-    installPath = Path(Context.getAutomationToolkitInstallPath())
-    sbsrender_exe = installPath.joinpath("sbsrender.exe")
-    sbsar = sbsarPath
+    return SBSARInfo(sbsarPath)
 
-    result: CompletedProcess = run([sbsrender_exe, "info", sbsar], capture_output=True)
-    infoByte = result.stdout.decode()
-    infos = tuple(map(lambda x: x.strip().split(' '), infoByte.strip().split('\r\n')))
-
-    sbsarInfo = dict()
-    currentGraph = None
-    for l in infos:
-        if l[0] == "GRAPH-URL":
-            _, graph = l[1].split('//')
-            d = {"INPUT": dict(), "OUTPUT": dict()}
-            currentGraph = d
-            sbsarInfo[graph] = d
-            continue
-        else:
-
-            if l[0] == "INPUT":
-                io, name, t = l
-                currentGraph[io][name] = t
-            elif l[0] == "OUTPUT":
-                lenL = len(l)
-                if lenL <= 1:
-                    continue
-                elif lenL == 3:
-                    io, name, t = l
-                    currentGraph[io][name] = t
-                    continue
-                elif lenL == 2:
-                    io, name = l
-                    currentGraph[io][name] = "NO_USAGE"
-                    continue
-                else:
-                    io, name, *usage = l
-                    currentGraph[io][name] = "MULTIPLE_USAGE"
-                    continue
-            elif l[0] == "PRESET":
-                io, name = l
-                if io not in currentGraph:
-                    currentGraph[io] = list()
-                currentGraph[io].append(name)
-            else:
-                print(l[0])
     return sbsarInfo
+
+
+if __name__ == '__main__':
+    print(GetSBSARInfo(r"C:\test\presetTest.sbsar"))
--- scripts/Submission/sbsrenderSubmit.py
+++ scripts/Submission/sbsrenderSubmit.py
@@ -10,7 +10,7 @@ from functools import partial
 from Deadline.Scripting import RepositoryUtils
 
 from SubmitDLJob import SubmitJob
-from sbsrenderInfoParser import GetSBSARInfo
+from sbsrenderInfoParser import GetSBSARInfo, SBSARInfo, GraphInfo
 
 submitter = partial(SubmitJob, "sbsrender")
 
@@ -28,12 +28,16 @@ g_output = None
 g_usageOn = None
 g_outBitDepth = None
 g_outName = None
+g_presets = None
+g_randomSeed = None
+g_diffEachSeed = None
 
 # params
 ## job
 
 
 ## plugin inputs
+
 ### deadline
 PARM_CSV = "parmCSVFile"
 ### sbsrender
@@ -45,6 +49,7 @@ OUTPUT = "output"
 OUTPUT_PATH = "output-path"
 OUTPUT_NAME = "output-name"
 OUTPUT_FORMAT = "output-format"
+PRESET = "use-preset"
 
 
 # todo : preset 추가
@@ -60,8 +65,9 @@ def exceptAll(func):
 
 
 def __main__(*args):
-    global g_input, g_dialog, g_inputGraph, g_usageOn, g_output, g_outBitDepth, g_outName
+    global g_input, g_dialog, g_inputGraph, g_usageOn, g_output, g_outBitDepth, g_outName, g_presets, g_randomSeed, g_diffEachSeed
     # Tab Start
+
     g_dialog.AddTabControl("Deadline Job Controls", 0, 0)
 
     # Tab Page(SBS Render Options) Start
@@ -70,35 +76,63 @@ def __main__(*args):
     g_dialog.AddControlToGrid("IOSep", "SeparatorControl", "I/O options", r := 0, c := 0, colSpan=5)
 
     g_dialog.AddControlToGrid(f"{INPUT}Label", "LabelControl", "SBSAR File", r := r + 1, c := 0, expand=False)
-    g_input = g_dialog.AddSelectionControlToGrid(INPUT, "FileBrowserControl", "", "Substance Archaive File (*.sbsar)", r, c := c + 1, colSpan=2)
+    g_input = g_dialog.AddSelectionControlToGrid(INPUT, "FileBrowserControl", "",
+                                                 "Substance Archaive File (*.sbsar)",
+                                                 r, c := c + 1, colSpan=2)
 
     g_dialog.AddControlToGrid(f"{INPUT_GRAPH}Label", "LabelControl", "Input Graph", r := r, c := c + 2)
-    g_inputGraph = g_dialog.AddComboControlToGrid(INPUT_GRAPH, "ComboControl", "", ("",), r, c := c + 1, expand=True)
-    g_inputGraph.currentTextChanged.connect(GraphChanged)
+    g_inputGraph = g_dialog.AddComboControlToGrid(INPUT_GRAPH, "ComboControl", "", ("",), r, c := c + 1,
+                                                  expand=True)
+    g_inputGraph.currentTextChanged.connect(OnGraphChanged)
 
     g_dialog.AddControlToGrid(f"{OUTPUT}Label", "LabelControl", "Outputs to export", r := r + 1, c := 0)
-    g_output = g_dialog.AddControlToGrid(OUTPUT, "TextControl", "", r, c := c + 1)
-    g_usageOn = g_dialog.AddSelectionControlToGrid("outputBasedUsage", "CheckBoxControl", False, "Output Based Usage", r := r, c := c + 1)
+    g_output = g_dialog.AddControlToGrid(OUTPUT, "TextControl", "", r, c := c + 1, colSpan=3)
+    g_usageOn = g_dialog.AddSelectionControlToGrid("outputBasedUsage", "CheckBoxControl", False,
+                                                   "Output Based Usage",
+                                                   r := r, c := c + 3)
     g_usageOn.toggled.connect(UsageToggled)
 
     g_dialog.AddControlToGrid(f"{PARM_CSV}Label", "LabelControl", "Parm CSV File", r := r + 1, c := 0, expand=False)
-    g_dialog.AddSelectionControlToGrid(PARM_CSV, "FileBrowserControl", "", "Comma Seperated Value (*.csv)", r, c := c + 1, colSpan=2)
+    g_dialog.AddSelectionControlToGrid(PARM_CSV, "FileBrowserControl", "", "Comma Seperated Value (*.csv)", r,
+                                       c := c + 1, colSpan=2)
+    g_dialog.AddControlToGrid(f"{PRESET}Label", "LabelControl", "Preset", r, c := c + 2)
+    g_presets = g_dialog.AddComboControlToGrid(PRESET, "ComboControl", "", ("",), r, c := c + 1)
 
-    g_dialog.AddControlToGrid(f"{OUT_DEPTH}Label", "LabelControl", "Output Bit Depth", r := r + 1, c := 0, expand=False)
-    g_outBitDepth = g_dialog.AddComboControlToGrid(OUT_DEPTH, "ComboControl", "16", ("8", "16", "16f", "32f"), r, c := c + 1)
+    g_dialog.AddControlToGrid(f"{OUT_DEPTH}Label", "LabelControl", "Output Bit Depth", r := r + 1, c := 0,
+                              expand=False)
+    g_outBitDepth = g_dialog.AddComboControlToGrid(OUT_DEPTH, "ComboControl", "16", ("8", "16", "16f", "32f"), r,
+                                                   c := c + 1)
 
-    g_dialog.AddControlToGrid(f"{OUTPUT_PATH}Label", "LabelControl", "Destination Path", r := r + 1, c := 0, expand=False)
+    g_dialog.AddControlToGrid(f"{OUTPUT_PATH}Label", "LabelControl", "Destination Path", r := r + 1, c := 0,
+                              expand=False)
     g_dialog.AddSelectionControlToGrid(OUTPUT_PATH, "FolderBrowserControl", "", "", r, c := c + 1, colSpan=4)
 
-    g_dialog.AddControlToGrid(f"{OUTPUT_NAME}Label", "LabelControl", "File Name Pattern", r := r + 1, c := 0, expand=False)
-    g_outName = g_dialog.AddControlToGrid(OUTPUT_NAME, "TextControl", "{inputGraphUrl}_{outputNodeName}", r, c := c + 1, colSpan=3, expand=True)
+    g_dialog.AddControlToGrid(f"{OUTPUT_NAME}Label", "LabelControl", "File Name Pattern", r := r + 1, c := 0,
+                              expand=False)
+    g_outName = g_dialog.AddControlToGrid(OUTPUT_NAME, "TextControl", "{inputGraphUrl}_{outputNodeName}", r,
+                                          c := c + 1,
+                                          colSpan=3, expand=True)
     g_dialog.AddComboControlToGrid(OUTPUT_FORMAT, "ComboControl", "png",
-                                   ("surface", "dds", "bmp", "jpg", "jif", "jpeg", "jpe", "png", "tga", "targa", "tif", "tiff", "wap", "wbmp", "wbm", "psd", "psb", "hdr", "exr", "webp"), r,
+                                   ("surface", "dds", "bmp", "jpg", "jif", "jpeg", "jpe", "png", "tga", "targa",
+                                    "tif",
+                                    "tiff", "wap", "wbmp", "wbm", "psd", "psb", "hdr", "exr", "webp"), r,
                                    c := 4)
 
-    g_dialog.AddControlToGrid("outputsizeLabel", "LabelControl", "Output Size(x, y)", r := r + 1, c := 0, expand=False)
-    g_dialog.AddRangeControlToGrid("outputsizeX", "SliderControl", 10, 0, 13, 0, 1, r, c := c + 1, colSpan=2)
-    g_dialog.AddRangeControlToGrid("outputsizeY", "SliderControl", 10, 0, 13, 0, 1, r, c := c + 2, colSpan=2)
+    g_dialog.AddControlToGrid("outputsizeLabel", "LabelControl", "Output Size(x, y)", r := r + 1, c := 0,
+                              expand=False)
+    sizeTuple = tuple([str(2 ** i) for i in range(0, 14)])
+
+    g_dialog.AddComboControlToGrid("outputsizeX", "ComboControl", "1024", sizeTuple, r, c := c + 1, colSpan=2)
+    g_dialog.AddComboControlToGrid("outputsizeY", "ComboControl", "1024", sizeTuple, r, c := c + 2, colSpan=2)
+
+    g_dialog.AddControlToGrid("randomseedLabel", "LabelControl", "Random seed", r := r + 1, c := 0, expand=False)
+    g_randomSeed = g_dialog.AddRangeControlToGrid("randomseed", "SliderControl", 0, 0, 2147483647 / (2 ** 7), 0, 1,
+                                                  r,
+                                                  c := c + 1, colSpan=2)
+    g_diffEachSeed = g_dialog.AddSelectionControlToGrid("diffEachSeed", "CheckBoxControl", False,
+                                                        "Randomize seed each time",
+                                                        r := r, c := c + 2, colSpan=2)
+    g_diffEachSeed.toggled.connect(lambda x: g_randomSeed.setEnabled(not x))
 
     g_dialog.EndGrid()
 
@@ -126,58 +160,67 @@ def __main__(*args):
     g_dialog.EndGrid()
     # End grid: Buttons
 
-    g_dialog.ShowDialog(True)
+    g_dialog.ShowDialog()
 
 
-@exceptAll
 def UsageToggled(toggled):
+    """
+    Slot for the usage checkbox.
+    Modify the output based on the usage.
+    :param toggled:
+    :return:
+    """
     global g_output, g_sbsarDict
     try:
-
-        if not os.path.exists(g_input.TheValue):
-            return
         currGraph = g_inputGraph.currentText()
         if currGraph == "":
             return
 
         g_output.clear()
-
         assert currGraph in g_sbsarDict, f"{currGraph} not in current SBSAR file."
 
         gDict = g_sbsarDict[currGraph]
-        assert "OUTPUT" in gDict, f"No output in {currGraph}"
 
-        USAGE_FAIL = ("NO_USAGE", "MULTIPLE_USAGE")
         usageOn = toggled
-        outputDict = gDict["OUTPUT"]
 
-        if usageOn:
-            usages = []
-            for k, v in outputDict.items():
-                if v in USAGE_FAIL:
-                    continue
-                usages.append(v)
-
-            txt = ' '.join(usages)
+        if usageOn:  # Usage based output
+            txt = ' '.join(gDict.Usage)
             g_output.setText(txt)
             g_outName.setText("{inputGraphUrl}_{outputUsages}")
 
-
-        else:
-            outputNames = []
-            for k in outputDict.keys():
-                outputNames.append(k)
-            txt = ' '.join(outputNames)
+        else:  # Name based output
+            txt = ' '.join(gDict.Output)
             g_output.setText(txt)
             g_outName.setText("{inputGraphUrl}_{outputNodeName}")
+
+
+
     except Exception as e:
         print(e)
 
 
-def GraphChanged(selectedGraph):
-    global g_sbsarDict, g_inputGraph, g_output
+def OnGraphChanged(newGraph=""):
+    """
+    Reset the output and preset based on the current graph.
+    Internally call UsageToggled to modify output.
+    """
+    global g_sbsarDict, g_inputGraph, g_output, g_presets
+    if newGraph == "":
+        return
+
     usageOn = g_usageOn.isChecked()
     UsageToggled(usageOn)
+
+    if g_sbsarDict.HasPreset:
+        g_presets.clear()
+        g_presets.addItem("No preset")
+        for p in g_sbsarDict.Preset:
+            g_presets.addItem(p)
+        g_presets.setEnabled(True)
+    else:
+        g_presets.clear()
+        g_presets.addItem("Not available")
+        g_presets.setDisabled(True)
 
 
 def OnSBSARInputChanged(args):
@@ -188,14 +231,17 @@ def OnSBSARInputChanged(args):
         g_sbsarDict = sbsInfo
 
     g_inputGraph.clear()
-    inputGraphs = tuple(g_sbsarDict.keys())
-    for k in inputGraphs:
-        g_inputGraph.addItem(k)
+    for f in dir(g_inputGraph):
+        print(f)
+    return
+    try:
+        g_inputGraph.addItem("Select")
+    except Exception as e:
+        print(e)
+        return
 
     return
-
-    usageOn = g_usageOn.isChecked()
-    UsageToggled(usageOn)
+    OnGraphChanged(g_sbsarDict.GraphNames[0])
 
 
 def Test():
